5a9070f43c1b475b98a45d725a204767b63d7fd7
Sadzurami <sadzurami2036@gmail.com>
2024-12-04 19:29:29 +0300

    feat: add options for message formatting

--- src/logger.ts
+++ src/logger.ts
@@ -1,17 +1,46 @@
 import chalk from 'chalk';
 
+import { LoggerOptions } from './types/logger.options.type';
+
 export class Logger {
-  constructor(private readonly name: string) {}
+  public readonly name: string;
+  public readonly options: LoggerOptions;
 
-  public info(...messages: any[]) {
-    console.log(new Date().toLocaleTimeString(), '-', chalk.inverse('info'), `[${this.name}]`, ...messages);
+  constructor(name: string, options?: LoggerOptions) {
+    this.name = name;
+    this.options = options || {};
   }
 
-  public warn(...messages: any[]) {
-    console.log(new Date().toLocaleTimeString(), '-', chalk.bgCyanBright('warn'), `[${this.name}]`, ...messages);
+  public info(...messages: string[]) {
+    const prefix = `${new Date().toLocaleTimeString()} - ${chalk.inverse('info')} [${this.name}]`;
+    const message = messages.join(' ');
+
+    console.log(this.formatMessage(`${prefix} ${message}`));
   }
 
-  public error(...messages: any[]) {
-    console.log(new Date().toLocaleTimeString(), '-', chalk.bgMagentaBright('error'), `[${this.name}]`, ...messages);
+  public warn(...messages: string[]) {
+    const prefix = `${new Date().toLocaleTimeString()} - ${chalk.bgCyanBright('warn')} [${this.name}]`;
+    const message = messages.join(' ');
+
+    console.log(this.formatMessage(`${prefix} ${message}`));
+  }
+
+  public error(...messages: string[]) {
+    const prefix = `${new Date().toLocaleTimeString()} - ${chalk.bgMagentaBright('error')} [${this.name}]`;
+    const message = messages.join(' ');
+
+    console.log(this.formatMessage(`${prefix} ${message}`));
+  }
+
+  private formatMessage(message: string): string {
+    if (this.options.maxStringLength) message = this.truncateMessage(message);
+
+    return message;
+  }
+
+  private truncateMessage(message: string): string {
+    if (message.length < this.options.maxStringLength) return message;
+
+    return message.slice(0, this.options.maxStringLength) + '...';
   }
 }
---
+++ src/types/logger.options.type.ts
@@ -0,0 +1,6 @@
+export type LoggerOptions = {
+  /**
+   * Set the maximum length of the displayed message.
+   */
+  maxStringLength?: number;
+};
